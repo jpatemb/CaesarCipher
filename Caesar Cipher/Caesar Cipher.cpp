27b3dc4015848f0932f50fd5c0f4eb409fa67e96
jpatemb <51932270+jpatemb@users.noreply.github.com>
2021-02-08 18:45:23 -0500

    Add files via upload

---
+++ Caesar Cipher/Caesar Cipher.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+
+using namespace std;
+
+
+int main() {
+	
+	int Height;
+	string Name;
+
+	Height = 7;
+	Name = "JP";
+
+	cout << Name << endl;
+	cout << Height << endl;
+
+	string A = "X";
+	string B = "Y";
+	string C = "Z";
+	string D = "A";
+	string E = "B";
+	string F = "C";
+	string G = "D";
+	string H = "E";
+	string I = "F";
+	string J = "G";
+	string K = "H";
+	string L = "I";
+	string M = "J";
+	string N = "K";
+	string O = "L";
+	string P = "M";
+	string Q = "N";
+	string R = "O";
+	string S = "P";
+	string T = "Q";
+	string U = "R";
+	string V = "S";
+	string W = "T";
+	string X = "U";
+	string Y = "V";
+	string Z = "W";
+
+	cout << K + H + O + O + R;
+
+
+
+
+	return 0;
+
+}
---
+++ Caesar Cipher/Caesar Cipher/Source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+
+	string A = "X";
+	string B = "Y";
+	string C = "Z";
+	string D = "A";
+	string E = "B";
+	string F = "C";
+	string G = "D";
+	string H = "E";
+	string I = "F";
+	string J = "G";
+	string K = "H";
+	string L = "I";
+	string M = "J";
+	string N = "K";
+	string O = "L";
+	string P = "M";
+	string Q = "N";
+	string R = "O";
+	string S = "P";
+	string T = "Q";
+	string U = "R";
+	string V = "S";
+	string W = "T";
+	string X = "U";
+	string Y = "V";
+	string Z = "W";
+
+	cout << KHOOR;
+
+
+
+
+	return 0;
+
+}
